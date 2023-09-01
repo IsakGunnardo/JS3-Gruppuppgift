c0cf0e4ba85b6d260d37bce10684d39c1eb3fbb9
Pjohanssonswe <p.johansson.swe@gmail.com>
2023-09-01 12:08:04 +0200

    fix async and states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
-import { getAllComments,  } from './api';
+import { getAllPosts, getAllComments, getAllUsers } from './api';
+import { useEffect, useState } from 'react';
+
+function PeopleList ({users}) {
+  return (
+    <ul>
+      {users.map((person, index) => (
+        <li key={index}>
+         {person.firstName}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
-  
-  return (
-    <div className="App">
+  //const [posts, setPosts] = useState([]);
+  //const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    </div>
+/*
+  useEffect(() => {
+    getAllPosts().then((result) => setPosts(result.posts));
+  }, []);
+
+  useEffect(() => {
+    getAllComments().then((result) => setComments(result.comments))
+  }, []);
+*/
+  useEffect(() => {
+    getAllUsers().then((result) => setUsers(result.users))
+  }, []);
+
+
+console.log(users[1])
+
+
+
+  return (
+    <>
+      <h1>Users</h1>
+      <PeopleList users={users} />
+    </>
   );
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,30 @@
+export async function getAllPosts() {
+  let result = await fetch("https://dummyjson.com/posts");
+  let json = await result.json();
+  return json;
+}
+
+
+export async function getAllComments() {
+  let result = await fetch("https://dummyjson.com/comments");
+  let json = await result.json();
+  return json;
+}
+
+
+export async function getAllUsers() {
+  let result = await fetch("https://dummyjson.com/users");
+  let json = await result.json();
+  return json;
+}
+
+
+
+
+
+
+
+/*
 import { useEffect, useState } from "react";
 //Försöka att skapa en 
 export function getAllComments({comments}) {
@@ -10,16 +37,7 @@ export function getAllComments({comments}) {
         let json = await result.json();
         return json;
     },[api]);
-    
-    
+      
 } 
+*/
 
-
-export async function getAllProducts() {
-    let result = await fetch("https://dummyjson.com/products");
-    let json = await result.json();
-    return json;
-  }
-  useEffect(() => {
-    getAllProducts().then((result) => setProducts(result.products));
-  }, []);
