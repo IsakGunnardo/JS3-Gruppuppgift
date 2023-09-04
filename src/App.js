2026e4809879e4c19877496234e7eb5ce159a63b
Pjohanssonswe <p.johansson.swe@gmail.com>
2023-09-04 09:40:30 +0200

    fix komponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 //import './App.css';
 import { getAllComments,  } from './api';
 import Navigator from './components/navigator';
+import { getAllPosts, getAllComments, getAllUsers } from './api';
+import { useEffect, useState } from 'react';
+
+function PeopleList ({users}) {
+  return (
+    <ul>
+      {users.map((person, index) => (
+        <li key={index}>
+         {person.firstName}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function PostKomponent ({posts}) {
+  return (
+    <ul>
+      {posts.map((holder, index) => (
+        <li key={index}>
+          {holder.body}</li>
+      ))}    
+    </ul>
+  );
+}
+
 function App() {
-  
+
+
+
+  const [posts, setPosts] = useState([]);
+  //const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    getAllPosts().then((result) => setPosts(result.posts));
+  }, []);
+
+  /*
+  useEffect(() => {
+    getAllComments().then((result) => setComments(result.comments))
+  }, []);
+*/
+  useEffect(() => {
+    getAllUsers().then((result) => setUsers(result.users))
+  }, []);
+
+
+console.log(posts[1])
+
   return (
     
     <>
