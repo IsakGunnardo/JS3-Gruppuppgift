d9af1caa5be2c9655ea41f87f1725876bae21935
Pjohanssonswe <p.johansson.swe@gmail.com>
2023-09-05 13:56:48 +0200

    update git

--- src/components/main.js
+++ src/components/main.js
@@ -20,7 +20,7 @@ export default function Main({ posts, users }) {
             </li>
           ))}
           {console.log(JSON.stringify(users[1]))}
-          {console.log(JSON.stringify(posts[1]))} 
+           
         </ul>
       </>
     );
